e5ad01746fb7001c56c3f529daac0f75953afcc5
Donovan Nye <donovan.nye@gmail.com>
2019-09-20 15:53:26 -0500

    flowify the store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+// @flow
+
 import {
   createStore,
   compose,
@@ -7,21 +9,39 @@ import {
 import thunk from 'redux-thunk';
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
+import type { Store, Action, Dispatch } from 'redux'; 
+import type { LazyStore } from 'pwa-helpers/lazy-reducer-enhancer.js';
+
 // Sets up a Chrome extension for time travel debugging.
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
 const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export type DreamState = {
+  ...
+};
+
+export type DreamAction = Action<string>;
+export type DreamStore = Store<DreamState, DreamAction, Dispatch> & LazyStore;
 
 // Initializes the Redux store with a lazyReducerEnhancer (so that you can
 // lazily add reducers after the store has been created) and redux-thunk (so
 // that you can dispatch async actions). See the "Redux and state management"
 // section of the wiki for more details:
 // https://github.com/Polymer/pwa-starter-kit/wiki/4.-Redux-and-state-management
-export const store = createStore(
-  state => state,
+export const store: DreamStore = dreamifyStore(createStore(
+  (state: any) => state,
   devCompose(
     lazyReducerEnhancer(combineReducers),
     applyMiddleware(thunk))
-);
+));
+
+// https://stackoverflow.com/a/41329247
+function dreamifyStore(reduxStore: any): DreamStore {
+  if (typeof reduxStore.addReducer === 'function') {
+    return (reduxStore: DreamStore);
+  }
+  throw 'Non-Hackible Store';
+}
 
 // Initially loaded reducers.
 store.addReducers({
